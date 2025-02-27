c4270d0e019530656dce070cffb769e7c75971ce
Emily Lockhart <validitymaiden@gmail.com>
2025-02-27 12:11:56 -0500

    add some packaging path info

--- language/paths.nix
+++ language/paths.nix
@@ -2,7 +2,9 @@ let
     absolute = /absolute/path;
     relative = relative/path;
     parent = ../.;
+    nixpkgs = <nixpkgs>;
+    nixpkgs_lib = <nixpkgs/lib>;
 in
 {
-    inherit absolute relative parent;
+    inherit absolute relative parent nixpkgs nixpkgs_lib;
 }
