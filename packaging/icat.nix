7f7ff9567ac48febaff706780103b1fe601fbdc8
Emily Lockhart <validitymaiden@gmail.com>
2025-03-01 11:14:44 -0500

    add a package for icat currently fails due to a dependency missing

---
+++ packaging/icat.nix
@@ -0,0 +1,18 @@
+# icat.nix
+{
+    stdenv,
+    fetchFromGitHub,
+    imlib2,
+}:
+
+stdenv.mkDerivation {
+    pname = "icat";
+    version = "v0.5";
+
+    src = fetchFromGitHub {
+        owner = "atextor";
+        repo = "icat";
+        rev = "v0.5";
+        sha256 = "0wyy2ksxp95vnh71ybj1bbmqd5ggp13x3mk37pzr99ljs9awy8ka";
+    };
+}
