bc559b50cee813823b372959942633887dae40dc
Emily Lockhart <validitymaiden@gmail.com>
2025-03-03 17:40:38 -0500

    use lib.fileset.intersection but specify the local files to include

--- local_files/fileset/build.nix
+++ local_files/fileset/build.nix
@@ -2,7 +2,15 @@
 let
   fs = lib.fileset;
   # needed fs.intersection since this is a subdirectory in an existing git repo
-  sourceFiles = fs.intersection ./. (fs.gitTracked ../..);
+  sourceFiles = fs.intersection
+    (fs.gitTracked ../..)
+    (fs.unions [
+        ./hello.txt
+        ./world.txt
+        ./build.sh
+        ./src
+    ])
+  ;
 in
 
 fs.trace sourceFiles
