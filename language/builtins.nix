61952a10556b424d92aadc9f8f113e95682f2d0d
Emily Lockhart <validitymaiden@gmail.com>
2025-07-17 13:39:09 -0400

    builtins.nix: - use lib.range rather than a static list

--- language/builtins.nix
+++ language/builtins.nix
@@ -1,21 +1,20 @@
 let
+  lib = import <nixpkgs/lib>;
   ex_toString = {
     a = 1;
     __toString = self: "a: " + toString self.a;
   };
   gt = x: y: y > x;
-  list_1_5 = [ 1 2 3 4 5];
 in
 builtins.trace ''
 
 
 ex_toString = { a = 1; __toString = self: ...; };
 gt = x: y: y > x;
-list_1_5 = [ 1 2 3 4 5];
 ''
 {
   "builtins.toString ex_toString" = builtins.toString ex_toString;
   "builtins.add 1 2" = builtins.add 1 2;
-  "builtins.all (gt 3) list_1_5" = builtins.all (gt 3) list_1_5;
-  "builtins.any (gt 3) list_1_5" = builtins.any (gt 3) list_1_5;
+  "builtins.all (gt 3) (lib.range 1 5)" = builtins.all (gt 3) (lib.range 1 5);
+  "builtins.any (gt 3) (lib.range 1 5)" = builtins.any (gt 3) (lib.range 1 5);
 }
