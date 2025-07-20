46d3df9219df88117f8ed8095c37f080308f5cfd
Emily Lockhart <validitymaiden@gmail.com>
2025-07-20 18:52:15 -0400

    builtins.nix: - add derivation example

--- language/builtins.nix
+++ language/builtins.nix
@@ -5,19 +5,31 @@ let
     __toString = self: "a: " + toString self.a;
   };
   gt = x: y: y > x;
+  derivationAttrs = {
+    name = builtins.baseNameOf ./.;
+    system = builtins.currentSystem;
+    builder = "/bin/seashell";
+  };
+  drv = derivation derivationAttrs;
 in
 builtins.trace ''
 
 
 ex_toString = { a = 1; __toString = self: ...; };
 gt = x: y: y > x;
+derivationAttrs = {
+  name = builtins.baseNameOf ./.;
+  system = builtins.currentSystem;
+  builder = "/bin/seashell";
+};
 ''
 {
-  #  derivation
-  #  import
-  #  abort
-  #  throw
   #  derivation attrs
+  "derivation derivationAttrs" = {
+    "drvPath" = drv.drvPath;
+    "drvAttrs" = drv.drvAttrs;
+  };
+
   #  abort s
   #  add e1 e2
   "builtins.add 1 2" = builtins.add 1 2;
